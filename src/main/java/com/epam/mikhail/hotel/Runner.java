ec5d9ef6a866cf1ba9436f8727e1f9d2a929fe90
mikhailsnitsarenko <liahim_s@mail.ru>
2016-02-12 06:07:43 +0600

    add get and set, equals and hashCode, toString

--- src/main/java/com/epam/mikhail/hotel/Runner.java
+++ src/main/java/com/epam/mikhail/hotel/Runner.java
@@ -1,11 +1,16 @@
 package com.epam.mikhail.hotel;
 
 
+import com.epam.mikhail.hotel.model.User;
+import com.epam.mikhail.hotel.model.factory.UserFactory;
+
 public class Runner {
 
 
     public static void main(String[] args) {
 
+        User user = UserFactory.createUser();
+        System.out.println(user);
     }
 
 }
--- src/main/java/com/epam/mikhail/hotel/model/Role.java
+++ src/main/java/com/epam/mikhail/hotel/model/Role.java
@@ -16,4 +16,38 @@ public class Role extends BaseEntity {
         super(uuid);
         this.title = title;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+        if (!super.equals(o)) return false;
+
+        Role role = (Role) o;
+
+        return title != null ? title.equals(role.title) : role.title == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "title='" + title + '\'' +
+                '}';
+    }
 }

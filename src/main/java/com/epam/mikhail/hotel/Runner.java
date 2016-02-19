a5990df9bc3c83d738cefde6effefddcb2c43bd8
mikhailsnitsarenko <liahim_s@mail.ru>
2016-02-19 14:01:06 +0600

    add compare

--- src/main/java/com/epam/mikhail/hotel/Runner.java
+++ src/main/java/com/epam/mikhail/hotel/Runner.java
@@ -2,15 +2,27 @@ package com.epam.mikhail.hotel;
 
 
 import com.epam.mikhail.hotel.model.Room;
+import com.epam.mikhail.hotel.model.compare.SortedRoomsById;
 import com.epam.mikhail.hotel.model.factory.RoomFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Runner {
 
 
     public static void main(String[] args) {
 
-        Room room = RoomFactory.createRoom();
-        System.out.println(room);
+        List<Room> rooms = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            Room room = RoomFactory.createRoom();
+            rooms.add(room);
+        }
+
+        rooms.forEach(System.out::println);
+        Collections.sort(rooms, new SortedRoomsById());
+        rooms.forEach(System.out::println);
     }
 
 }
